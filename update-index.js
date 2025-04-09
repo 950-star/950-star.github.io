070a9d9f2e530f6f8c81dd939b482304b9ccfdc8
950-star <karanpa521@gmail.com>
2025-04-09 22:00:16 +0530

    jjnm

--- update-index.js
+++ update-index.js
@@ -74,7 +74,7 @@ function generateIndexForFolder(folderPath, relativePath, parentFolderName = 'Pa
     <meta charset="UTF-8">
     <title>${folderName}</title>
     <link rel="stylesheet" href="/styles.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 </head>
 <body>
